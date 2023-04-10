2671079e4093c7b9740ad20e4ad5b021f972d390
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-10 14:06:47 -0500

    added walkthrough.gif

--- src/components/CrewmateCard.jsx
+++ src/components/CrewmateCard.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-import crewmateImg from "../assets/crewmate.ce385016.png"
+import crewmate from "../assets/crewmate.png"
 
 const CrewmateCard = ({name, speed, color, id}) => {
     return (
         <div className="crewmate-card" id={color}>
-            <img src={crewmateImg} className="single-crewmate"></img>
+            <img src={crewmate} className="single-crewmate"></img>
             <h3>Name: {name}</h3>
             <h3>Speed: {speed} MPH</h3>
             <h3>Color: {color}</h3>
